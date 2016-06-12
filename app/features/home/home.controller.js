a418dcd96a228f558082c2a97b38b648555b4fcb
porajustyna <justynapora@interia.pl>
2016-06-12 21:45:55 +0000

    Dodawanie obsługi przycisku dodaj

--- app/features/home/home.controller.js
+++ app/features/home/home.controller.js
@@ -2,5 +2,18 @@ export default class HomeController {
   constructor($scope, $firebaseArray) {
    var  ref = new Firebase("https://projektangular1.firebaseio.com");
 $scope.lista = $firebaseArray (ref);
-  }
+
+$scope.dodajtaska = function() {
+              var message_ref = new Firebase('https://projektangular1.firebaseio.com/Zadania');
+              var newMessageRef = message_ref.push();
+              newMessageRef.set({
+                  'Email': $scope.email,
+                  'Tresc': $scope.task,
+                  'Status': 'w realizacji'
+              
+});
+
+};
+
+}
 }
