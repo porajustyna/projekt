20c2d4850fb5ef1673363c48d4ef0753e8677368
porajustyna <justynapora@interia.pl>
2016-06-13 18:52:29 +0000

    Dodanie walidacji

--- app/features/home/home.controller.js
+++ app/features/home/home.controller.js
@@ -7,9 +7,16 @@ $scope.lista = $firebaseArray (ref);
 
 
 $scope.zalogujsie = function() {
-    $scope.emailwer=$scope.email;
+     var walidacja = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+      if ($scope.email == '' || !walidacja.test($scope.email)){
+    alert("Błąd ! Wpisz poprawny adres email");
+   
+}
+else{
+     $scope.emailwer=$scope.email;
     $rootScope.sprmail = true;
     
+}
 
 };
 
